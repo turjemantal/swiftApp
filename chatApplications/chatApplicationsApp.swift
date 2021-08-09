c28f95ceaa89641515a0ecf2e0418563f03407c7
turjemantal <turjemantal20@gmail.com>
2021-08-10 01:06:38 +0300

    Initial Commit

---
+++ chatApplications/chatApplicationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatApplicationsApp.swift
+//  chatApplications
+//
+//  Created by Tal Turjeman on 10/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct chatApplicationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
